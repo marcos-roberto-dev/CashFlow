be26cfc470c74ff3c64b4d711eb5c5608f4dd5a9
Marcos Roberto <marcosprojetsdev@gmail.com>
2025-02-16 19:19:17 -0300

    :feat: add PDF report generation for expenses and implement font resolver

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -0,0 +1,17 @@
+using System;
+using PdfSharp.Fonts;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+
+public class ExpensesReportFontResolver : IFontResolver
+{
+    public byte[]? GetFont(string faceName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,14 +1,22 @@
-using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Repositories.Expenses;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 
-public class GenerateExpensesReportPdfUseCase(IExpenseReadOnlyRepository repository) : IGenerateExpensesReportPdfUseCase
+public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
 {
+    private readonly IExpenseReadOnlyRepository _repository;
     private const string CURRENCY_SYMBOL = "$";
+
+    public GenerateExpensesReportPdfUseCase(IExpenseReadOnlyRepository repository)
+    {
+        _repository = repository;
+        GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
+    }
     public async Task<byte[]> Execute(DateOnly month)
     {
-        var expenses = await repository.FilterByMonth(month);
+        var expenses = await _repository.FilterByMonth(month);
         
         if(expenses.Count == 0)
         {

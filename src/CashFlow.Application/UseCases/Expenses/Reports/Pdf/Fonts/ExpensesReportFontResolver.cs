9620ac8625a7b14b34bb17963352bf84affab336
Marcos Roberto <marcosprojetsdev@gmail.com>
2025-02-17 07:25:26 -0300

    :feat: implement font loading in ExpensesReportFontResolver and add DEFAULT_FONT constant in FontHelper

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Reflection;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
@@ -7,11 +7,28 @@ public class ExpensesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
-        throw new NotImplementedException();
+        var stream = ReadFontFile(faceName);
+       
+        stream ??= ReadFontFile(FontHelper.DEFAULT_FONT);
+
+        var length = (int)stream!.Length;
+
+        var data = new byte[length];
+
+        stream.ReadExactly(buffer: data);
+
+        return data;
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{faceName}.ttf");
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -6,4 +6,5 @@ public static class FontHelper
     public const string RELEWAY_BLACK = "Raleway-Black";
     public const string WORK_SANS_REGULAR = "Worksans-Regular";
     public const string WORK_SANS_BLACK = "Worksans-Black";
+    public const string DEFAULT_FONT = RELEWAY_REGULAR;
 }
